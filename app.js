6cb40caaa02d286a9a1eecbf4418c85fdb74b1a6
sac6inarya <sachinkarya333@gmail.com>
2023-08-18 23:48:46 +0530

    sync js definitions

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-const db = require("./util/database");
+const sequelize = require("./util/database");
 
 const app = express();
 
@@ -22,6 +22,12 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000, () => {
-  console.log("Server listening at 3000");
-});
+sequelize
+  .sync()
+  .then((res) => {
+    // console.log(res);
+    app.listen(3000, () => {
+      console.log("Server listening at 3000");
+    });
+  })
+  .catch((err) => console.error(err));
