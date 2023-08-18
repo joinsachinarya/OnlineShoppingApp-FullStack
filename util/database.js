eeff17b21286ce6cac39deee27f9d7be7a2efdc6
sac6inarya <sachinkarya333@gmail.com>
2023-08-18 23:26:48 +0530

    connecting with database using sequelize

--- util/database.js
+++ util/database.js
@@ -1,9 +1,6 @@
-const mysql = require("mysql2");
-
-const pool = mysql.createPool({
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("sys", "root", "sachinandarya", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "sys",
-  password: "sachinandarya",
 });
-module.exports = pool.promise();
+module.exports = sequelize;
