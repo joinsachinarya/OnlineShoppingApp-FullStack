c055cbfef7da18e60db132d139ac7b6991322564
sac6inarya <sachinkarya333@gmail.com>
2023-08-18 22:25:00 +0530

    fetching products

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,13 +2,17 @@ const Product = require("../models/product");
 const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("shop/product-list", {
-      prods: products,
-      pageTitle: "All Products",
-      path: "/products",
+  Product.fetchAll()
+    .then(([rows, fieldData]) => {
+      res.render("shop/product-list", {
+        prods: rows,
+        pageTitle: "All Products",
+        path: "/products",
+      });
+    })
+    .catch((err) => {
+      console.error(err);
     });
-  });
 };
 
 exports.getProduct = (req, res, next) => {
