ac7b1d816e50145328cc6c7f112e43fd3905a84a
sac6inarya <sachinkarya333@gmail.com>
2023-08-18 21:44:53 +0530

    retrive data from database

--- app.js
+++ app.js
@@ -14,7 +14,13 @@ app.set("views", "views");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-db.execute("SELECT * FROM products");
+db.execute("SELECT * FROM products")
+  .then((result) => {
+    console.log(result[0]);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -24,4 +30,6 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Server listening at 3000");
+});
