b7de15f1ba1015d44a098eaa04ec617ba68024ad
sac6inarya <sachinkarya333@gmail.com>
2023-08-20 20:50:21 +0530

    fix typo

--- util/database.js
+++ util/database.js
@@ -1,8 +1,8 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
+const sequelize = new Sequelize("sys", "root", "sachinandarya", {
+  dialect: "mysql",
+  host: "localhost",
 });
 
 module.exports = sequelize;
