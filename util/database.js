26da2c845e175e8d4da8126c1a53cce54b70e650
sac6inarya <sachinkarya333@gmail.com>
2023-08-29 09:32:18 +0530

    optimize db connection code

--- util/database.js
+++ util/database.js
@@ -1,8 +1,28 @@
-const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize("sys", "root", "sachinandarya", {
-  dialect: "mysql",
+const { Sequelize } = require("sequelize");
+const dbConfig = {
+  database: "shoppingDB",
+  usrename: "root",
+  password: "Password!@#$",
   host: "localhost",
-});
+  dialect: "mysql",
+};
+const sequelize = new Sequelize(
+  dbConfig.database,
+  dbConfig.usrename,
+  dbConfig.password,
+  {
+    dialect: dbConfig.dialect,
+    host: dbConfig.host,
+  }
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Server authenticated database.");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 module.exports = sequelize;
